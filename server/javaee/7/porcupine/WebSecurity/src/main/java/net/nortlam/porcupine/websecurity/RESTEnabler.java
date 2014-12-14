cef0d086104300a96f82e6a9323b625a85d4dd15
Mauricio "Maltron" Leal <maltron@gmail.com>
2014-12-14 14:59:29 -0200

    Deleting not necessary subprojects

--- server/javaee/7/porcupine/WebSecurity/src/main/java/net/nortlam/porcupine/websecurity/RESTEnabler.java
+++
@@ -1,9 +0,0 @@
-package net.nortlam.porcupine.websecurity;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class RESTEnabler extends Application {
-
-}
