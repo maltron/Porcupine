c231b6055f0e9076e6add60c23a29971fb877d35
Mauricio "Maltron" Leal <maltron@gmail.com>
2014-12-14 16:44:10 -0200

    Create new projects for decoupling purposes

---
+++ server/javaee/7/porcupine/sample-clients/sample-client2/src/main/java/net/nortlam/sample/RESTEnabler.java
@@ -0,0 +1,11 @@
+package net.nortlam.sample;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Mauricio "Maltron" Leal */
+@ApplicationPath("/rest")
+public class RESTEnabler extends Application {
+}
